85dbebe4bdf7eb1db29eb6b6943e0f9c196928b4
psteadman <patrick.steadman@mail.utoronto.ca>
2015-02-17 22:53:55 -0500

    Updated examples in README.md and added details to MWM table

--- README.md
+++ README.md
@@ -21,36 +21,45 @@ Requires sqlite3 - most computers should have this installed already but if not 
 
 ##### Setting up your sqlite database
 
-```bash
+``` sh
 sqlite3 databasename.db < filename.sql
 ```
 
 ##### mousedb.py usage
 
-```bash
+``` sh
 mousedb.py [options] database.db
 ```
 
 ##### To access help run: 
 
-```
+``` sh
 mousedb.py --help
 ```
 
 #### Populating your database
 
 1. Add a new study to your database
-
+    This is used to add a new study within the mouse database
+    Example:
+    ``` sh
+    mousedb.py -s --studyid CREB-hippocampus --mouseprefix CREBHippo
+    ```
 
 2. Add mice to your study
     This is required to tell the database the name of each mouse that will be involved with a study.
     Example: 
-    
+    ``` sh
+    mousedb.py -m --studyid StudyName --sex M --breeder 550540 --dob 20141127 --cage 557884 --genotype WT mydatabase.db
+    mousedb.py -m --mouseid Study1_0022 --injections='Tamoxifen, 1, 21.9' (drug, # injections, weight at each injection)
+    ```
 
 3. Add a training experiment
     This is required to tell the database that a mouse has undergone a behavioural training experiment and the characteristics of that session. 
     Example:
-    
+    ``` sh
+    mousedb.py franklandpsteadman.db -t --ttype watermaze --startdate 20150217 --protocol='6 trial/dy, 3 days' --trainingid 2015-004 --mouseid Study1_0020
+    ```
 
 4. Add a training experiment's data
     This is where you upload data from a behaviour experiment, whether it is fear conditioning, rotarod, watermaze, etc. into the database
--- database.sql
+++ database.sql
@@ -85,6 +85,36 @@ select raise(abort, 'Update on table "Rotarod" violates Foreign Key Constraint "
 where (SELECT MouseID FROM Mouse WHERE MouseID = NEW.MouseID) is null;
 end;
 
+CREATE TABLE MWM (
+MouseID TEXT NOT NULL, --# From Mouse table
+TrainingID TEXT NOT NULL, --# From Training table
+Day INTEGER,
+Trial INTEGER,
+Platform INTEGER,
+TimeToPlatform REAL,
+TrialDuration REAL,
+DistanceTravelcm REAL, --# unit?
+AverageSpeedcms REAL, --# unit?
+ZonePctTime1 REAL,
+ZonePctTime2 REAL,
+ZonePctTime3 REAL,
+ZonePctTime4 REAL,
+FOREIGN KEY (TrainingID) REFERENCES Training(TrainingID),
+FOREIGN KEY (MouseID) REFERENCES Mouse(MouseID)); 
+
+CREATE TRIGGER MWMInsert
+before insert on MWM
+for each row begin
+select raise(abort, 'Insert on table "MWM" violates Foreign Key Constraint "MWMInsert" - ensure mouse is in database')
+where (SELECT MouseID FROM Mouse WHERE MouseID = NEW.MouseID) is null;
+end;
+CREATE TRIGGER MWMUpdate
+before update on MWM
+for each row begin
+select raise(abort, 'Update on table "MWM" violates Foreign Key Constraint "MWMUpdate" - ensure mouse is in database')
+where (SELECT MouseID FROM Mouse WHERE MouseID = NEW.MouseID) is null;
+end;
+
 CREATE TABLE FC (
 MouseID TEXT NOT NULL, --# From Mouse table
 TrainingID TEXT NOT NULL, --# From Training table
@@ -92,115 +122,15 @@ TrainingID TEXT NOT NULL, --# From Training table
 FOREIGN KEY (TrainingID) REFERENCES Training(TrainingID),
 FOREIGN KEY (MouseID) REFERENCES Mouse(MouseID)); 
 
-CREATE TABLE MWM (
-MouseID TEXT NOT NULL, --# From Mouse table
-TrainingID TEXT NOT NULL, --# From Training table
-
-FOREIGN KEY (TrainingID) REFERENCES Training(TrainingID),
-FOREIGN KEY (MouseID) REFERENCES Mouse(MouseID)); 
-
-CREATE TABLE BarnesTrial (
-MouseID TEXT NOT NULL, --# Mouse Name
-TrainingID TEXT NOT NULL, --# Mouse's training ID (first, reverse, etc.)
-Condition TEXT NOT NULL, --# Mouse's condition for that training
-TotalTrial INTEGER, --# Mouse's trail number (1-30 under current protocol)
-Day INTEGER, --# Day number
-Trial INTEGER, --# Mouse's trial of Day X
-DistanceMovedTotalcm REAL, --# Metric of learning
-DistanceToTargetMeancm REAL, --# Metric of learning
-DistanceToTargetTotalcm REAL, --# Metric of learning
-HeadingMean REAL, --# Metric of learning
-VelocityMean REAL, --# Metric of learning
-DurationInNonTarget REAL, --# Metric of learning
-FrequencyInNonTarget REAL, --# Metric of learning
-LatencyHeadDirectToTarget REAL, --# Metric of learning
-LatencyToTarget REAL, --# Metric of learning
-VideoFile TEXT, --# file path
-TrackingFile TEXT, --# file path
-Trainer TEXT, --# trainer
-Protocol TEXT,
-FOREIGN KEY (TrainingID) REFERENCES Training(TrainingID),
-FOREIGN KEY (MouseID) REFERENCES Mouse(MouseID)); --#The Training Protocol utilized
-
-CREATE TRIGGER BarnesTrialInsert
-before insert on BarnesTrial
+CREATE TRIGGER FCInsert
+before insert on FC
 for each row begin
-select raise(abort, 'Insert on table "BarnesTrial" violates Foreign Key Constraint "BarnesTrialInsert"')
+select raise(abort, 'Insert on table "FC" violates Foreign Key Constraint "FCInsert" - ensure mouse is in database')
 where (SELECT MouseID FROM Mouse WHERE MouseID = NEW.MouseID) is null;
 end;
-CREATE TRIGGER BarnesTrialUpdate
-before update on BarnesTrial
+CREATE TRIGGER FCUpdate
+before update on FC
 for each row begin
-select raise(abort, 'Update on table "BarnesTrial" violates Foreign Key Constraint "BarnesTrialUpdate"')
+select raise(abort, 'Update on table "FC" violates Foreign Key Constraint "FCUpdate" - ensure mouse is in database')
 where (SELECT MouseID FROM Mouse WHERE MouseID = NEW.MouseID) is null;
 end;
-
--- This table may need to be altered as k and beta may not stay and what other columns should be added?
-CREATE TABLE BarnesBehaviour (
-MouseID TEXT NOT NULL, --# mouse name
-TrainingID TEXT NOT NULL, --# Training (first, reverse, etc.)
-Beta REAL, --# Beta for a exponential fit of the LatencyToTarget Metric
-K REAL,
-FOREIGN KEY (TrainingID) REFERENCES Training(TrainingID),
-FOREIGN KEY (MouseID) REFERENCES Mouse(MouseID));
-
-CREATE TRIGGER BarnesBehaviourInsert
-before insert on BarnesBehaviour
-for each row begin
-select raise(abort, 'Insert on table "BarnesBehaviour" violates Foreign Key Constraint "BarnesBehaviourInsert"')
-where (SELECT MouseID FROM Mouse WHERE MouseID = NEW.MouseID) is null;
-end;
-
-CREATE TRIGGER BarnesBehaviourUpdate
-before update on BarnesBehaviour
-for each row begin
-select raise(abort, 'Update on table "BarnesBehaviour" violates Foreign Key Constraint "BarnesBehaviourUpdate"')
-where (SELECT MouseID FROM Mouse WHERE MouseID = NEW.MouseID) is null;
-end;
-
-CREATE TABLE Scan (
-MouseID TEXT NOT NULL, --# Mouse name
-Day TEXT, --# Relative Day of scan (Day 1, 2, 5, 15, etc)
-TrueDay TEXT, --# Relative Day of scan (Day 0 = sunday before training)
-Coil TEXT, --# Coil mouse was scanned in
-Filename TEXT, --# Filename of reconstructed image
-nScan TEXT, --# What scan number is it for this mouse?
-ScanProtocol TEXT, --# What sequence was used?
-Weight TEXT, --# Mouse weight in g
-Operator TEXT, --# Who conducted the scan
-ScanDate TEXT,
-'Image Quality Metric' real, --# added August 26th 2010
-Notes text, --# added August 26th 2010
-Billed text, --# added August 26th 2010
-DaysSinceLastScan text, --# added October 8th 2010
-AgeAtScan real, --# added October 8th 2010
-ReversalDay text, --# added October 8th 2010
-TrueReversalDay real, --# added October 8th 2010
-Modality text, --# added June 10th 2011
-PerfusionDate text, --# added August 30th 2011
-FOREIGN KEY (MouseID) REFERENCES Mouse(MouseID)); --# Date of scan yyyymmdd
-
-CREATE TRIGGER ScanInsert
-before insert on Scan
-for each row begin
-select raise(abort, 'Insert on table "Scan" violates Foreign Key Constraint "ScanInsert"')
-where (SELECT MouseID FROM Mouse WHERE MouseID = NEW.MouseID) is null;
-end;
-CREATE TRIGGER ScanUpdate
-before update on Scan
-for each row begin
-select raise(abort, 'Update on table "Scan" violates Foreign Key Constraint "ScanUpdate"')
-where (SELECT MouseID FROM Mouse WHERE MouseID = NEW.MouseID) is null;
-end;
-
--- Removed this trigger as does not seem to be working correctly - gave error when I tried to delete mouse even though there should have been no other conflict - October 15, 2010
---CREATE TRIGGER MouseDelete
---before delete on Mouse
---for each row begin
---select raise(abort, 'Delete on table "Mouse" violates Foreign Key Constraint "MouseDelete" MouseID for this delete is used in other tables still, remove to proceed.')
---where ((SELECT MouseID FROM Training WHERE MouseID = OLD.MouseID) is null)
---or ((SELECT MouseID FROM Scan WHERE MouseID = OLD.MouseID) is null)
---or ((SELECT MouseID FROM BarnesTrial WHERE MouseID = OLD.MouseID) is null)
---or ((SELECT MouseID FROM BarnesBehaviour WHERE MouseID = OLD.MouseID) is null);
---end;
-
